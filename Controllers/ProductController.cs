c3afe47506a780908cd2c03885a54c88de8c5d9b
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-06-11 09:40:07 -0300

    feat: ProductController criado. Iniciando configuração do SqlServer

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -14,8 +14,57 @@ namespace Shop.Controllers
       [FromServices] DataContext context
     )
     {
-      List<Product> products = await context.Products.AsNoTracking().ToListAsync();
+      List<Product> products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
       return Ok(products);
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<List<Product>>> GetById(
+      [FromServices] DataContext context,
+      int id
+      )
+    {
+      Product? product = await context.Products.Include(x => x.Category).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+      return Ok(product);
+    }
+
+    [HttpGet]
+    [Route("categories/{id:int}")]
+    public async Task<ActionResult<List<Product>>> GetByCategory(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      List<Product> product = await context.Products
+      .Include(x => x.Category)
+      .AsNoTracking()
+      .Where(x => x.CategoryId == id)
+      .ToListAsync();
+
+      return Ok(product);
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Product>> Post(
+    [FromBody] Product model,
+    [FromServices] DataContext context)
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      try
+      {
+        context.Products.Add(model);
+        await context.SaveChangesAsync();
+        return Ok(model);
+      }
+      catch (System.Exception)
+      {
+        return BadRequest(new { message = "Algo de errado aconteceu, tente novamente mais tarde" });
+      }
     }
   }
 }
