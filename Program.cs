e6aed2d23521fe68f1b8007cb82bd1b71d50dfb3
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-06-11 10:03:07 -0300

    feat: Configuração de banco finalizada

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ using Shop.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+//builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+builder.Services.AddDbContext<DataContext>(
+  opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"))
+);
 builder.Services.AddScoped<DataContext, DataContext>();
 
 builder.Services.AddEndpointsApiExplorer();
